763d8236e8c81957053c4bcf7edb2c3229cdc170
Viktor Pyvovarov <viktor.pyvovarov@cloudlock.com>
2017-05-09 15:01:48 +0300

    StT work

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import argparse
+from lib.audio import mp3_to_flack
+from lib.speech_to_text import transcribe_streaming, transcribe_sync
+
+# async def main():
+#     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+#     parser.add_argument('stream', help='File to stream to the API')
+#     args = parser.parse_args()
+#     async for audio_chunck in mp3_to_flack(args.stream):
+#         async for data in transcribe_streaming(audio_chunck, 44100):
+#             print(data)
+#
+#
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(main())
+
+async def main(loop):
+    from concurrent.futures import ThreadPoolExecutor
+    e = ThreadPoolExecutor()
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('stream', help='File to stream to the API')
+    args = parser.parse_args()
+    async for audio_chunck in mp3_to_flack(args.stream):
+        print('---> NEW CHUNCK  <-----')
+        loop.run_in_executor(e, transcribe_sync, audio_chunck, 44100)
+
+loop = asyncio.get_event_loop()
+task = loop.create_task(main(loop))
+ret = loop.run_until_complete(task)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(name='ellipsis',
+      version='0.1',
+      description='',
+      author='viktor.pyvovarov',
+      author_email='v.s.pyvovarov@gmail.com',
+      url='',
+      packages=find_packages(),
+      install_requires=[
+          "google-cloud",
+          "pydub"
+      ],
+      )
