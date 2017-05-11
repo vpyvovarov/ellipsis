5b1c2b0eb36e99dd145303684e9dc22463d085cf
Viktor Pyvovarov <viktor.pyvovarov@cloudlock.com>
2017-05-11 08:18:38 +0300

    Add basic translation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import argparse
 from lib.audio import mp3_to_flack
+from lib.translate import translate_text
 from lib.speech_to_text import transcribe_streaming, transcribe_sync
 
 # async def main():
@@ -16,6 +17,10 @@ from lib.speech_to_text import transcribe_streaming, transcribe_sync
 # loop = asyncio.get_event_loop()
 # loop.run_until_complete(main())
 
+def tr(audio_chunck):
+    text, conf = transcribe_sync(audio_chunck, 44100)
+    translate_text("ru", text)
+
 async def main(loop):
     from concurrent.futures import ThreadPoolExecutor
     e = ThreadPoolExecutor()
@@ -24,7 +29,7 @@ async def main(loop):
     args = parser.parse_args()
     async for audio_chunck in mp3_to_flack(args.stream):
         print('---> NEW CHUNCK  <-----')
-        loop.run_in_executor(e, transcribe_sync, audio_chunck, 44100)
+        loop.run_in_executor(e, tr, audio_chunck)
 
 loop = asyncio.get_event_loop()
 task = loop.create_task(main(loop))
