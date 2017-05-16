7015daaa18737e41d38ca5ff64fa5db185be1e8c
Viktor Pyvovarov <viktor.pyvovarov@cloudlock.com>
2017-05-16 19:15:52 +0300

    Use Watson for text to speech

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import asyncio
 import argparse
+from os.path import join, dirname
+
 from lib.audio import mp3_to_flack
+from lib.text_to_speech_ibm import WatsonApi
 from lib.translate import translate_text
 from lib.speech_to_text import transcribe_streaming, transcribe_sync
+
 
 # async def main():
 #     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -17,20 +21,34 @@ from lib.speech_to_text import transcribe_streaming, transcribe_sync
 # loop = asyncio.get_event_loop()
 # loop.run_until_complete(main())
 
-def tr(audio_chunck):
+def tr(audio_chunck, watson_user, watson_pass):
     text, conf = transcribe_sync(audio_chunck, 44100)
-    translate_text("ru", text)
+    translation = translate_text("en", text)
+    output_file = join(dirname(__file__), 'output1.wav')
+    WatsonApi(username=watson_user, password=watson_pass).synthesize_speach(text=translation, output_file=output_file)
+
 
 async def main(loop):
     from concurrent.futures import ThreadPoolExecutor
     e = ThreadPoolExecutor()
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('stream', help='File to stream to the API')
+    parser.add_argument('watson_user', help='User for Watson API')
+    parser.add_argument('watson_pass', help='Password for Watson API')
+
     args = parser.parse_args()
     async for audio_chunck in mp3_to_flack(args.stream):
         print('---> NEW CHUNCK  <-----')
-        loop.run_in_executor(e, tr, audio_chunck)
+
+        loop.run_in_executor(e, tr, audio_chunck, args.watson_user, args.watson_pass)
 
 loop = asyncio.get_event_loop()
 task = loop.create_task(main(loop))
 ret = loop.run_until_complete(task)
+
+
+# loop.run_until_complete(asyncio.gather(
+#     factorial("A", 2),
+#     factorial("B", 3),
+#     factorial("C", 4),
+# ))
