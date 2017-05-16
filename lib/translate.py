dd3a025e7413fa86bce1002d745970ab4006ca38
Viktor Pyvovarov <viktor.pyvovarov@cloudlock.com>
2017-05-16 18:54:26 +0300

    Update translation method

--- lib/translate.py
+++ lib/translate.py
@@ -1,6 +1,6 @@
 from google.cloud import translate
 
-def translate_text(target, text):
+def translate_text(target, text, model=translate.NMT):
     """Translates text into the target language.
 
     Target must be an ISO 639-1 language code.
@@ -13,13 +13,13 @@ def translate_text(target, text):
 
     # Text can also be a sequence of strings, in which case this method
     # will return a sequence of results for each text.
-    result = translate_client.translate(
-        text, target_language=target)
+    result = translate_client.translate(text, target_language=target,  model=model)
 
     print(u'Text: {}'.format(result['input']))
     print(u'Translation: {}'.format(result['translatedText']))
     print(u'Detected source language: {}'.format(
         result['detectedSourceLanguage']))
+    return result['translatedText']
 
 if __name__ == "__main__":
-    translate_text("ru", "Somebody like it hot")
+    translate_text("en", "Somebody like it hot")
