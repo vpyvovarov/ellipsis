c144127a54eca509c8df71ab3b5ba7ce1fa0435d
Viktor Pyvovarov <viktor.pyvovarov@cloudlock.com>
2017-05-30 16:23:47 +0300

    Initial config class

--- lib/conf.py
+++ lib/conf.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import os
 import yaml
+from lib.log import get_logger
 
 
 def load_test_config(config_file='config.yaml'):
@@ -10,12 +11,23 @@ def load_test_config(config_file='config.yaml'):
         with open(path) as f:
             config = yaml.load(f)
     except IOError:
-        config = None
+        raise RuntimeError("Can't load config at %s" % package_root)
     return config
-
 
 class Config:
     def __init__(self):
         config = load_test_config()
+        self.config = ConfBuilder(config)
+        self.log = get_logger(context='Config')
+
+class ConfBuilder:
+    def __init__(self, config):
         for key in config:
-            setattr(self, key, config[key])
+            value = config[key]
+            if issubclass(type(value), dict):
+                value = ConfBuilder(value)
+            setattr(self, key, value)
+
+
+if __name__ == "__main__":
+    a = Config()
